6f77ddc8e4f24e4d16a30f4c399e33c9b667ad26
ABukSwienty <alexanderbukswienty@gmail.com>
2022-10-19 10:16:01 +0200

    fix(info-card): badge colors

--- src/library/components/atoms/info-card/info-card-badge.tsx
+++ src/library/components/atoms/info-card/info-card-badge.tsx
@@ -3,13 +3,13 @@ import setClasses from "../../../utils/set-classes";
 
 export const infoBadgeColors: SpringColors = {
   brand: "bg-brand-700 border border-brand-800 text-white",
-  secondary: "",
-  accent: "",
-  success: "",
-  error: "",
-  warning: "",
-  light: "",
-  dark: "",
+  secondary: "bg-white border border-brand-200 text-brand-900",
+  accent: "bg-accent-700 border border-accent-800 text-white",
+  success: "bg-success-700 border border-success-800 text-white",
+  error: "bg-error-700 border border-error-800 text-white",
+  warning: "bg-warning-700 border border-warning-800 text-white",
+  light: "bg-gray-100 border border-gray-200 text-gray-900",
+  dark: "bg-gray-800 border border-gray-900 text-white",
 };
 
 export interface InfoCardBadgeProps {
